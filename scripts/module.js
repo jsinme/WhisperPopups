1f71ad94ae5f144ac608de1234097afab743e111
Jason <jason@jsin.me>
2021-01-27 22:25:13 -0500

    Testing how to register settings

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,23 @@
 Hooks.once('init', async function() {
-
+    registerSettings();
 });
 
 Hooks.once('ready', async function() {
 
 });
+
+
+
+function registerSettings() {
+    game.settings.register("whisper-popup", "enableWhisperPopups", {
+        name: "Display Popups",
+        hint: "Set whether or not to display popups when receiving whispers",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => {
+            console.log(value)
+        }
+    });
+}
