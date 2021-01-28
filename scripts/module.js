9deb0fa6995a00cf73b577793c23201993f56c75
Jason Singh <jason@jsin.me>
2021-01-28 11:30:09 -0500

    Hooked onto createChatMessage and succesfuly logged when the message is a whisper and the user is in the recipient list

--- scripts/module.js
+++ scripts/module.js
@@ -1,23 +1,23 @@
-Hooks.once('init', async function() {
-    registerSettings();
+Hooks.once("init", async function () {
+  registerSettings();
 });
 
-Hooks.once('ready', async function() {
-
+Hooks.on("createChatMessage", (message) => {
+  if (message.data.whisper.length > 0 && message.data.whisper.find((el) => el == game.userId) != undefined) {
+    console.log("Success");
+  }
 });
-
-
 
 function registerSettings() {
-    game.settings.register("whisper-popup", "enableWhisperPopups", {
-        name: "Display Popups",
-        hint: "Set whether or not to display popups when receiving whispers",
-        scope: "world",
-        config: true,
-        type: Boolean,
-        default: false,
-        onChange: value => {
-            console.log(value)
-        }
-    });
+  game.settings.register("whisper-popup", "enableWhisperPopups", {
+    name: "Display Popups",
+    hint: "Set whether or not to display popups when receiving whispers",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: false,
+    onChange: (value) => {
+      console.log(value);
+    },
+  });
 }
